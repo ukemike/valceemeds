b753eeceb092ec5cf3d0f371828a1befdcfc8ba2
ukemike <ukemike4@gmail.com>
2021-11-19 11:06:34 +0100

    Finished logo style

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const faqRoutes = require("./routes/faqs")
 
 const MongoDBStore = require("connect-mongo")(session);
 
-// const dbUrl = "mongodb://localhost:27017/valceemeds";
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/valceemeds';
+const dbUrl = "mongodb://localhost:27017/valceemeds";
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/valceemeds';
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
